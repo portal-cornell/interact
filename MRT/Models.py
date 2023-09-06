6daeab1132e4d508ef72f5b31619f656a18eaef2
Prithwish Dan <dan.prithwish@gmail.com>
2023-09-06 17:46:52 -0400

    Defined intent informed forecaster

--- MRT/Models.py
+++ MRT/Models.py
@@ -131,6 +131,135 @@ class Decoder(nn.Module):
             return dec_output, dec_slf_attn_list, dec_enc_attn_list
         return dec_output, dec_enc_attn_list
 
+class IntentInformedForecaster(nn.Module):
+    def __init__(
+            self, src_pad_idx=1, trg_pad_idx=1,
+            d_word_vec=64, d_model=64, d_inner=512,
+            n_layers=3, n_head=8, d_k=32, d_v=32, 
+            dropout=0.2, n_position=100, 
+            conditional_forecaster=False,
+            alice_joints_num = 9,
+            bob_joints_list = None,
+            bob_joints_num = 9,
+            one_hist = False,
+            device='cuda'):
+    
+        super().__init__()
+        self.conditional_forecaster = conditional_forecaster
+        self.one_hist = one_hist
+        self.bob_joint_indices = None
+        if bob_joints_list is not None:
+            joint_idx_size = len(bob_joints_list) * 3
+            bob_joint_indices = np.zeros(joint_idx_size, dtype=int)
+            for i, value in enumerate(bob_joints_list):
+                for j in range(3):
+                    bob_joint_indices[i*3+j] = value*3 + j
+            self.bob_joint_indices = bob_joint_indices
+        
+        self.device=device
+        
+        self.d_model=d_model
+        self.src_pad_idx, self.trg_pad_idx = src_pad_idx, trg_pad_idx
+
+        self.alice_local_hist_encoder=nn.Linear(alice_joints_num*3,d_model) 
+
+        self.alice_global_hist_encoder=nn.Linear(alice_joints_num*3,d_model)
+        self.bob_global_hist_encoder=nn.Linear(bob_joints_num*3,d_model) 
+
+        self.bob_global_future_encoder=nn.Linear(bob_joints_num*3,d_model) 
+        
+        self.decoder_linear = nn.Linear(d_model,d_model)
+        self.linear_proj_to_forecast = nn.Linear(1,15)
+        self.forecast_head=nn.Linear(d_model,alice_joints_num*3)
+
+        self.dropout = nn.Dropout(p=dropout)
+
+        self.encoder = Encoder(
+            n_position=n_position,
+            d_word_vec=d_word_vec, d_model=d_model, d_inner=d_inner,
+            n_layers=n_layers, n_head=n_head, d_k=d_k, d_v=d_v,
+            pad_idx=src_pad_idx, dropout=dropout, device=self.device)
+        
+        self.encoder_global = Encoder(
+            n_position=n_position,
+            d_word_vec=d_word_vec, d_model=d_model, d_inner=d_inner,
+            n_layers=n_layers, n_head=n_head, d_k=d_k, d_v=d_v,
+            pad_idx=src_pad_idx, dropout=dropout, device=self.device)
+
+        self.decoder = Decoder(
+            n_position=n_position,
+            d_word_vec=d_word_vec, d_model=d_model, d_inner=d_inner,
+            n_layers=n_layers, n_head=n_head, d_k=d_k, d_v=d_v,
+            pad_idx=trg_pad_idx, dropout=dropout, device=self.device)
+
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p) 
+
+        assert d_model == d_word_vec, \
+        'To facilitate the residual connections, \
+         the dimensions of all module outputs shall be the same.'
+    
+    def forward(self, 
+            alice_hist, 
+            bob_hist, 
+            bob_future,
+            add_spe=True, 
+            bob_is_robot=False):
+        ### This should be zero
+        alice_current_pos = alice_hist[:, -1, :].unsqueeze(1)
+        
+        ### Index out bob's relevant joints
+        if self.bob_joint_indices is not None:
+            bob_future = bob_future[:,:,self.bob_joint_indices]
+            bob_hist = bob_hist[:,:,self.bob_joint_indices]
+
+        ### Intent informed forecasting only cares about Bob's position at final timestep
+        bob_future = bob_future[:,-1:]
+        
+        ### local history encoding
+        alice_displacement = alice_hist[:,1:alice_hist.shape[1],:]-alice_hist[:,:alice_hist.shape[1]-1,:]                 
+        alice_displacement_dct = dct.dct(alice_displacement)
+        alice_local_enc = self.alice_local_hist_encoder(alice_displacement_dct)
+        alice_local_output, *_ = self.encoder(alice_local_enc, 1, None)
+
+        ### global history encoding
+        alice_global_enc = self.alice_global_hist_encoder(alice_hist)
+        if not self.one_hist:
+            bob_global_enc = self.bob_global_hist_encoder(bob_hist)
+            global_enc = torch.cat([alice_global_enc, bob_global_enc],dim=1)
+        else:
+            global_enc = alice_global_enc
+        global_output, *_ = self.encoder_global(global_enc,
+                    1 if self.one_hist else 2, 
+                    src_mask = None, 
+                    global_feature=True)
+
+        ### conditional future encoder
+        if not self.conditional_forecaster:
+            bob_future.fill_(0)
+        bob_cond_future_enc = self.bob_global_future_encoder(bob_future)
+
+        spe = 0
+        if add_spe:
+            alice_spe = torch.norm(alice_hist-alice_hist[:, -1].unsqueeze(1), dim=-1)
+            bob_spe = torch.norm(bob_hist-alice_hist[:, -1, self.bob_joint_indices].unsqueeze(1), dim=-1)
+            spe = torch.exp(-torch.cat([alice_spe, bob_spe] if not self.one_hist else [alice_spe], dim=1)).unsqueeze(2)
+
+        encoder_output = torch.cat([alice_local_output, global_output+spe], dim=1)
+
+        dec_output, dec_attention, *_ = self.decoder(bob_cond_future_enc, None, encoder_output, None)
+        dec_output = self.decoder_linear(dec_output)
+        dec_output = torch.permute(dec_output, (0,2,1)) # (batch size, 1, d_model) -> (batch size, d_model, 1)
+        dec_output = self.linear_proj_to_forecast(dec_output) # (batch size, d_model, 1) -> (batch size, d_model, 15)
+        dec_output = torch.permute(dec_output, (0,2,1)) # (batch size, d_model, 15) -> (batch size, 15, d_model)
+        alice_forecasts_dct = self.forecast_head(dec_output)
+        alice_forecasts_displacments = dct.idct(alice_forecasts_dct)
+
+        alice_forecasts = torch.cumsum(alice_forecasts_displacments, dim=1)
+
+        return alice_forecasts
+
 class ConditionalForecaster(nn.Module):
     def __init__(
             self, src_pad_idx=1, trg_pad_idx=1,
---
+++ train_intent_forecaster.py
@@ -0,0 +1,132 @@
+import torch
+import torch.optim as optim
+import numpy as np
+import torch_dct as dct #https://github.com/zh217/torch-dct
+import time
+
+from MRT.Models import IntentInformedForecaster
+from utils.loss_funcs import mpjpe_loss,disc_l2_loss,adv_disc_l2_loss
+from torch.autograd import Variable
+import torch.nn as nn
+from torch.nn import init
+import pathlib
+from arg_parser import get_parser
+from torch.utils.tensorboard import SummaryWriter
+
+from utils.cmu_mocap import CMU_Mocap
+from utils.synthetic_amass import Synthetic_AMASS
+from torch.utils.data import ConcatDataset, DataLoader
+
+dataset_map = {
+    'CMU-Mocap' : {},
+    'AMASS' : {}
+}
+
+def get_dataloader(split='train', batch_size=256, include_amass=True, include_CMU_mocap=True):
+    datalst = []
+    if include_CMU_mocap:
+        if split not in dataset_map['CMU-Mocap']:
+            dataset_map['CMU-Mocap'][split] = CMU_Mocap(split=split)
+        datalst.append(dataset_map['CMU-Mocap'][split])
+    if include_amass:
+        if split not in dataset_map['AMASS']:
+            dataset_map['AMASS'][split] = Synthetic_AMASS(split=split)
+        datalst.append(dataset_map['AMASS'][split])
+    dataset = ConcatDataset(datalst)
+    dataloader = DataLoader(dataset, 
+                batch_size=batch_size, 
+                shuffle=True if split == 'train' else False)
+    return dataloader
+
+def log_metrics(dataloader, split, writer, epoch):
+    total_loss, n=0, 0
+    model.eval()
+    with torch.no_grad():
+        for j, batch in enumerate(dataloader):
+            # [b.to(device) for b in batch]
+            offset = batch[0].reshape(batch[0].shape[0], 
+                                        batch[0].shape[1], -1)[:, -1].unsqueeze(1)
+            alice_hist, alice_fut, bob_hist, bob_fut = [(b.reshape(b.shape[0], 
+                                        b.shape[1], -1) - offset).to(device) for b in batch]
+            alice_forecasts = model(alice_hist, bob_hist, bob_fut)
+
+            loss = mpjpe_loss(alice_forecasts, alice_fut)
+            batch_dim = alice_hist.shape[0]
+            total_loss+=loss*batch_dim
+            n+=batch_dim
+    
+    print(f"{split} loss after epoch {epoch+1} = ", total_loss.item()/n)
+    writer.add_scalar(f'{split}/mpjpe', total_loss.item()/n, epoch+1)
+
+if __name__ == "__main__":
+    args = get_parser().parse_args()
+
+    ONE_HIST = args.one_hist
+    CONDITIONAL = args.conditional
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    model_id = f'{"1hist" if ONE_HIST else "2hist"}_{"marginal" if not CONDITIONAL else "conditional"}'
+    model_id += f'_{"noAMASS" if args.no_amass else "withAMASS"}_{"handwrist" if args.bob_hand else "alljoints"}'
+    writer = SummaryWriter(log_dir=args.log_dir+'/'+model_id)
+    
+    train_dataloader = get_dataloader(split='train', batch_size=args.batch_size, include_amass=(not args.no_amass))
+    val_dataloader = get_dataloader(split='val', batch_size=args.batch_size, include_amass=True, include_CMU_mocap=True)
+    test_dataloader = get_dataloader(split='test', batch_size=args.batch_size, include_amass=True, include_CMU_mocap=True)
+    amass_dataloader = get_dataloader(split='test', batch_size=args.batch_size, include_amass=True, include_CMU_mocap=False)
+    cmu_mocap_dataloader = get_dataloader(split='test', batch_size=args.batch_size, include_amass=False, include_CMU_mocap=True)
+    bob_joints_list = list(range(9)) if not args.bob_hand else list(range(5,9))
+
+    model = IntentInformedForecaster(d_word_vec=128, d_model=128, d_inner=1024,
+                n_layers=3, n_head=8, d_k=64, d_v=64,
+                device=device,
+                conditional_forecaster=CONDITIONAL,
+                bob_joints_list=bob_joints_list,
+                bob_joints_num=len(bob_joints_list),
+                one_hist=ONE_HIST).to(device)
+
+    params = [
+        {"params": model.parameters(), "lr": args.lr_pred}
+    ]
+
+    optimizer = optim.Adam(params,weight_decay=1e-05)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, 
+                milestones=[15,25,35,40], 
+                gamma=0.1)
+
+    directory = f'./checkpoints_new/saved_model_{model_id}'
+    pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
+
+    for epoch in range(args.epochs):
+        total_loss, n=0, 0
+        model.train()
+        for j, batch in enumerate(train_dataloader):
+            offset = batch[0].reshape(batch[0].shape[0], 
+                                        batch[0].shape[1], -1)[:, -1].unsqueeze(1)
+            alice_hist, alice_fut, bob_hist, bob_fut = [(b.reshape(b.shape[0], 
+                                        b.shape[1], -1) - offset).to(device) for b in batch]
+            alice_forecasts = model(alice_hist, bob_hist, bob_fut)
+            loss = mpjpe_loss(alice_forecasts, alice_fut)
+                
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            batch_dim = alice_hist.shape[0]
+            total_loss+=loss*batch_dim
+            n+=batch_dim
+
+        print(f"train loss after epoch {epoch+1} = ", total_loss.item()/n)
+        writer.add_scalar('train/mpjpe', total_loss.item()/n, epoch+1)
+        if (epoch+1)%3 == 0:
+            log_metrics(val_dataloader, 'val', writer, epoch)
+            log_metrics(test_dataloader, 'test', writer, epoch)
+            log_metrics(amass_dataloader, 'amass_test', writer, epoch)
+            log_metrics(cmu_mocap_dataloader, 'cmu_mocap_test', writer, epoch)
+
+        # if (epoch+1)%5==0:
+        
+        save_path=f'{directory}/{epoch+1}.model'
+        torch.save(model.state_dict(),save_path)
+        scheduler.step()
+    
